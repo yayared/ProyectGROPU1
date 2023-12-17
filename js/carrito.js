2299da0dab2cf6ff3b16f8fb638679ec742676b1
yayared <nighjard@gmail.com>
2023-12-17 17:57:40 -0500

    se suben estilos al carrito

--- js/carrito.js
+++ js/carrito.js
@@ -21,10 +21,19 @@ function mostrarProductosEnCarrito() {
 
         productoDiv.innerHTML = `<img src="${imagenSrc}" alt="${producto.nombre}">
                                  <p>${producto.nombre}</p>
-                                 <p>Precio: $${producto.costo.toFixed(2)}</p>
+                                 <p>Precio unitario: $${producto.costo.toFixed(2)}</p>
+                                 <label for="cantidad-${producto.nombre}">Cantidad:</label>
+                                 <input type="number" id="cantidad-${producto.nombre}" value="${producto.cantidad || 1}" min="1" max="10">
+                                 <p>Precio total: $${(producto.costo * (producto.cantidad || 1)).toFixed(2)}</p>
                                  <button onclick="eliminarProducto('${producto.nombre}')">Eliminar</button>
-                                 <button onclick="comprarProducto('${producto.nombre}', ${producto.costo})">Comprar</button>`;
+                                 <button onclick="comprarProducto('${producto.nombre}', ${producto.costo}, ${producto.cantidad || 1})">Comprar</button>`;
         carritoContainer.appendChild(productoDiv);
+
+        // Agregar evento para actualizar el precio total al cambiar la cantidad
+        const cantidadInput = document.getElementById(`cantidad-${producto.nombre}`);
+        cantidadInput.addEventListener("change", function () {
+            actualizarPrecioTotal(producto, cantidadInput.value);
+        });
     });
 }
 
@@ -33,17 +42,17 @@ function eliminarProducto(nombreProducto) {
     const carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
 
     const nuevoCarrito = carrito.filter(producto => producto.nombre !== nombreProducto);
-    
+
     sessionStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
 
     mostrarProductosEnCarrito();
 }
 
 // Función para comprar un producto individualmente
-function comprarProducto(nombreProducto, costoProducto) {
+function comprarProducto(nombreProducto, costoProducto, cantidad) {
     // Realizar cualquier acción necesaria al comprar un producto
-    const mensajeCompra = `Producto comprado: ${nombreProducto} con costo $${costoProducto.toFixed(2)}`;
-    
+    const mensajeCompra = `Producto comprado: ${nombreProducto} (Cantidad: ${cantidad}) con costo $${(costoProducto * cantidad).toFixed(2)}`;
+
     // Mostrar una alerta con la información del producto comprado
     alert(mensajeCompra);
 
@@ -51,12 +60,29 @@ function comprarProducto(nombreProducto, costoProducto) {
     eliminarProducto(nombreProducto);
 }
 
+// Función para actualizar el precio total al cambiar la cantidad
+function actualizarPrecioTotal(producto, nuevaCantidad) {
+    const carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
+
+    // Encontrar el producto en el carrito y actualizar la cantidad
+    const nuevoCarrito = carrito.map(p => {
+        if (p.nombre === producto.nombre) {
+            return { ...p, cantidad: parseInt(nuevaCantidad) };
+        }
+        return p;
+    });
+
+    sessionStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
+
+    mostrarProductosEnCarrito();
+}
+
 // Función para comprar todos los productos en el carrito
 function comprarTotal() {
     const carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
-    
+
     // Calcular el precio total de la compra
-    const precioTotal = carrito.reduce((total, producto) => total + producto.costo, 0);
+    const precioTotal = carrito.reduce((total, producto) => total + producto.costo * (producto.cantidad || 1), 0);
 
     // Realizar cualquier acción necesaria al comprar todos los productos
     const mensajeCompraTotal = `Compra total realizada. Precio total: $${precioTotal.toFixed(2)}`;
