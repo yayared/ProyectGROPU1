242de10064fd8f32223a36dc342052ee34b219ff
boyd10 <129134236+boyd10@users.noreply.github.com>
2023-12-16 22:35:31 -0500

    agregado el axios

--- js/tarjeta.js
+++ js/tarjeta.js
@@ -1,7 +1,18 @@
 const carlos_api ="http://localhost:5530/api/productos/nombre/"
 
 function guardar(){
-    axios
+    axios.post(carlos_api)
+        .then(function (response) {
+            
+            console.log('Datos guardados con éxito:', response.data);
+            alert('Datos de tarjeta de crédito guardados con éxito');
+        })
+        .catch(function (error) {
+            
+            console.error('Error al guardar los datos:', error);
+            alert('Error al guardar los datos de tarjeta de crédito');
+        });
+  }
     
 
 
@@ -10,4 +21,3 @@ function guardar(){
 
 
 
-}
