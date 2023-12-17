6ae414d6dc9dee46ab9bcc0a5ceb34e57d4e19d7
boyd10 <129134236+boyd10@users.noreply.github.com>
2023-12-17 02:53:35 -0500

    axios

--- js/tarjeta.js
+++ js/tarjeta.js
@@ -1,23 +1,33 @@
-const carlos_api ="http://localhost:5530/api/productos/nombre/"
+const carlos_api = "http://localhost:5530/api/ventas";
 
-function guardar(){
-    axios.post(carlos_api)
-        .then(function (response) {
-            
-            console.log('Datos guardados con éxito:', response.data);
-            alert('Datos de tarjeta de crédito guardados con éxito');
-        })
-        .catch(function (error) {
-            
-            console.error('Error al guardar los datos:', error);
-            alert('Error al guardar los datos de tarjeta de crédito');
-        });
-  }
-    
+function guardar() {
+    const informacion = {
+        NombreCompleto:  nombres.value,
+        Email: emaill.value,
+        Direccion:  direcion.value,
+        Ciudad: ciuda.value,
+        Estado:  estadu.value,
+        CodigoPostal:  postai.value,
+        TitularDeLaTarjeta: titular_tarjeta.value,
+        NumeroDeTarjeta:  numero_tarjeta.value,
+        Departamento:  departaments.value,
+        Vencimiento:  vencim.value,
+        CVV: codigo_12.value
+    };
 
-
-
-
-
+    axios.post(carlos_api, informacion, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function (response) {
+        console.log('Datos guardados con éxito:', response.data);
+        alert('Datos de tarjeta de crédito guardados con éxito');
+    })
+    .catch(function (error) {
+        console.error('Error al guardar los datos:', error);
+        alert('Error al guardar los datos de tarjeta de crédito');
+    });
+}
 
 
